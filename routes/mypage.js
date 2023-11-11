d1275e0c0e814b3960d4364e37fda7fce68dec41
EunnyKoo <kooeun55555@gmail.com>
2023-11-11 16:48:27 +0900

    add: (mypage) html, css 추가 / controller, routes 수정

--- routes/mypage.js
+++ routes/mypage.js
@@ -3,5 +3,10 @@ const mypageRouter = express.Router();
 const controller = require("../controller/CMypage");
 
 mypageRouter.get('/', controller.mypage);
+mypageRouter.get('/myfav', controller.myfav);
+mypageRouter.get('/myinfo', controller.myinfo);
+mypageRouter.get('/mymovielike', controller.mymovielike);
+mypageRouter.get('/mycommentlike', controller.mycommentlike);
+mypageRouter.get('/mycomment', controller.mycomment);
 
 module.exports = mypageRouter
