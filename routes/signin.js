b5830e80789ec086514acef46d4fe4d8a5e14c84
heoyunu <hyoon516@naver.com>
2023-11-15 04:01:19 +0900

    update:CSignin, signin.js, signin.ejs(로그인 관련 컨트롤러 및 라우터 추가+ signin.ejs에 axios 기능 추가

--- routes/signin.js
+++ routes/signin.js
@@ -1,7 +1,8 @@
-const express = require("express");
+const express = require('express');
 const signinRouter = express.Router();
-const controller = require("../controller/CSignin");
+const controller = require('../controller/CSignin');
 
 signinRouter.get('/', controller.signin);
+signinRouter.post('/', controller.user_signin);
 
 module.exports = signinRouter;
