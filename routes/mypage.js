e59f7f54703dd27cbe45e93413fdb8bf41490486
yunuheo <hyoon516@naver.com>
2023-11-18 13:41:41 +0900

    update: 회원탈퇴 기능 추가, user테이블 컬럼수정

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const mypageRouter = express.Router();
-const controller = require("../controller/CMypage");
+const controller = require('../controller/CMypage');
 
 mypageRouter.get('/', controller.mypage);
 mypageRouter.get('/myfav', controller.myfav);
@@ -8,7 +8,9 @@ mypageRouter.get('/myinfo', controller.myinfo);
 mypageRouter.get('/mymovielike', controller.mymovielike);
 mypageRouter.get('/mycommentlike', controller.mycommentlike);
 mypageRouter.get('/mycomment', controller.mycomment);
-//코멘트 삭제하기 위한 루터
+//코멘트 삭제하기 위한 라루터
 mypageRouter.delete('/mycomment/:id', controller.delete_comment);
+//회원탈퇴 라우터
+mypageRouter.post('/', controller.delete_user);
 
-module.exports = mypageRouter
+module.exports = mypageRouter;
