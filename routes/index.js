416f228b4be8007f44d257cdc2739475c527d917
riverhye <uhzigani@gmail.com>
2023-11-20 00:16:23 +0900

    update: MAIN section 1 DONE, seperate HEADER, new dummy data, restructre files

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/CMain");
+const api = require("../controller/CGetMovies");
 
 router.get("/", controller.main);
-
-router.post("/", controller.key);
 
 router.get("/header", controller.header);
 router.get("/footer", controller.footer);
 
-router.get("/saveMovies", controller.get_api_movies);
+router.get("/saveMovies", api.get_api_movies);
 
 module.exports = router;
