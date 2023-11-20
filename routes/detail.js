35f85c26fd987d6679eecf5efe6d1f569ec68c71
blee94 <135qudwls@naver.com>
2023-11-20 22:19:58 +0900

    update: detail, main

--- routes/detail.js
+++ routes/detail.js
@@ -2,13 +2,14 @@ const express = require('express');
 const detailRouter = express.Router();
 const controller = require('../controller/CDetail');
 
+detailRouter.get('/', controller.detail);
 // 영화 정보 가져오기
-detailRouter.get('/getMovieInfo/:movieidx', controller.getMovieInfo);
+// detailRouter.get('/getMovieInfo/:movieidx', controller.getMovieInfo);
 
 // 영화 리뷰 가져오기
-detailRouter.get('/getReviews', controller.getReviews);
+// detailRouter.get('/getReviews', controller.getReviews);
 
 // 영화 리뷰 저장하기
-detailRouter.post('/saveReview', controller.saveReview);
+// detailRouter.post('/saveReview', controller.saveReview);
 
 module.exports = detailRouter;
