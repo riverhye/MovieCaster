810c1b2db7c04921b3b2bee020f17047cb7a72a4
blee94 <135qudwls@naver.com>
2023-11-21 17:20:31 +0900

    update: detail

--- controller/CDetail.js
+++ controller/CDetail.js
@@ -1,25 +1,34 @@
-const { Movie_info, User, Comment } = require('../model');
+const { Movie_info, User, Comment } = require("../model");
 
-Comment.belongsTo(User, { foreignKey: 'useridx' });
+Comment.belongsTo(User, { foreignKey: "useridx" });
 
 exports.detail = (req, res) => {
-  res.render('detail', { movieInfo: null });
+  res.render("detail", { movieInfo: null });
 };
 
 exports.saveReview = async (req, res) => {
+  console.log("aaaaaaaaaaaaaaaa");
   try {
     const { movieIdx } = req.params;
-    const { rating, review } = req.body;
+    const { rating, description } = req.body;
+    const currentUserIdx = req.session.useridx;
+
+    if (!currentUserIdx) {
+      return res.status(401).json({ error: "사용자가 로그인하지 않았습니다." });
+    }
+
     // Comment 모델을 사용하여 데이터베이스에 새로운 리뷰 저장
     const newReview = await Comment.create({
-      movieIdx: movieIdx,
+      movieidx: movieIdx,
       rate: rating,
-      description: review,
+      description: description,
+      useridx: currentUserIdx,
     });
 
     // 클라이언트에 새로운 리뷰 데이터 전송
     res.status(200).json({ newReview });
   } catch (error) {
-    res.status(500).json({ error: '컨트롤러에서 저장실패' });
+    console.log(error);
+    res.status(500).json({ error: "컨트롤러에서 저장실패" });
   }
 };
--- routes/detail.js
+++ routes/detail.js
@@ -1,28 +1,29 @@
-const express = require('express');
+const express = require("express");
 const detailRouter = express.Router();
-const controller = require('../controller/CDetail');
-const { Movie_info, Comment, User, Op } = require('../model');
+const controller = require("../controller/CDetail");
+const { Movie_info, Comment, User } = require("../model");
+const { Op } = require("sequelize");
 
 // detailRouter.get('/:movieidx', controller.getMovieInfo);
 
 // '/detail/:movieidx'에 대한 GET 요청 처리
-detailRouter.get('/:movieidx', async (req, res) => {
+detailRouter.get("/:movieidx", async (req, res) => {
   try {
     const movieIdx = req.params.movieidx;
-    console.log('요청한 무비 인덱스: ', movieIdx);
+    console.log("요청한 무비 인덱스: ", movieIdx);
 
     const movieInfo = await Movie_info.findByPk(movieIdx, {
-      attributes: ['title', 'overview', 'release_date', 'poster_path', 'genre'],
+      attributes: ["title", "overview", "release_date", "poster_path", "genre"],
     });
 
     // 댓글과 사용자 정보를 가져오는 쿼리
     const comments = await Comment.findAll({
-      attributes: ['rate', 'description'],
+      attributes: ["rate", "description"],
       where: { movieidx: movieIdx },
       include: [
         {
           model: User,
-          attributes: ['nickname'],
+          attributes: ["nickname"],
         },
       ],
     });
@@ -30,26 +31,45 @@ detailRouter.get('/:movieidx', async (req, res) => {
     // 비슷한 장르의 영화 3개를 찾는 쿼리
     let similarMovies = [];
     if (movieInfo && movieInfo.genre) {
-      similarMovies = await Movie_info.findAll({
-        attributes: ['title', 'poster_path'],
-        where: {
-          genre: movieInfo.genre,
-          movieidx: { $not: movieIdx }, // 현재 영화와 같은 영화는 제외. field에 포함된 장르가 모두 일치해야해서 하나도 못불러오는건가?? 내일 해보기.
-        },
-        limit: 3,
-      });
+      const currentMovieGenres = movieInfo.genre.split(",");
+
+      // 각 장르에 대해 비슷한 영화를 찾아서 배열에 추가하는 부분
+      for (const genre of currentMovieGenres) {
+        const moviesWithGenre = await Movie_info.findAll({
+          attributes: ["title", "poster_path"],
+          where: {
+            genre: {
+              [Op.like]: `%${genre}%`, // 해당 장르를 포함하는 영화 검색
+            },
+            movieidx: { [Op.not]: movieIdx },
+          },
+          limit: 3,
+        });
+
+        similarMovies = similarMovies.concat(moviesWithGenre);
+      }
+
+      // 중복된 영화 제거
+      similarMovies = similarMovies.filter(
+        (movie, index, self) => index === self.findIndex((m) => m.movieidx === movie.movieidx)
+      );
     }
 
     // movieInfo, comments, similarMovies를 detail.ejs에 전달하여 렌더링
-    res.render('detail', { movieInfo, comments, similarMovies });
+    res.render("detail", {
+      movieInfo,
+      comments,
+      similarMovies,
+      useridx: req.session.useridx,
+    });
   } catch (error) {
-    console.error('Error fetching movie information:', error);
-    res.status(500).send('Error fetching movie information');
+    console.error("Error fetching movie information:", error);
+    res.status(500).send("Error fetching movie information");
   }
 });
 
-detailRouter.post('/:movieIdx', controller.saveReview);
+detailRouter.post("/:movieIdx", controller.saveReview);
 
-detailRouter.get('/', controller.detail);
+detailRouter.get("/", controller.detail);
 
 module.exports = detailRouter;
