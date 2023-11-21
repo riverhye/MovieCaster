462db9d46db67d19dfa544bef99a3170aae80f8f
EunnyKoo <kooeun55555@gmail.com>
2023-11-21 16:48:45 +0900

    update:mypage controller,ejs

--- routes/mypage.js
+++ routes/mypage.js
@@ -3,6 +3,7 @@ const mypageRouter = express.Router();
 const controller = require('../controller/CMypage');
 
 mypageRouter.get('/', controller.mypage);
+
 mypageRouter.get('/myfav', controller.myfav);
 mypageRouter.get('/myinfo', controller.myinfo);
 mypageRouter.get('/mymovielike', controller.mymovielike);
@@ -14,12 +15,10 @@ mypageRouter.post('/', controller.delete_user);
 mypageRouter.post('/mypage/update', controller.update_profile);
 //코멘트 삭제하기 위한 라루터
 mypageRouter.delete('/mycomment/:id', controller.delete_comment);
-//코멘트 수정하기 위한 라우터
-mypageRouter.put('/mycomment/:id', controller.update_comment);
 //내가 좋아요 누른 코멘트 삭제하기 위한 라우터
 mypageRouter.delete('/mycommentlike/:id', controller.delete_comment_like);
 //내가 좋아요 누른 영화 삭제하기 위한 라우터
 mypageRouter.delete('/mymovielike/:id', controller.delete_movie_like);
-//인생영화 검색 
+//인생영화 등록
 mypageRouter.post('/myfav/add', controller.addFavMovie);
 module.exports = mypageRouter;
