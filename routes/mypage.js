29ed6c3bc64f64b9664e8efe06fa101ad9c5431d
heoyunu <hyoon516@naver.com>
2023-11-22 13:26:34 +0900

    update: index.js, Cmypage, mypage*.ejs -이미지등록  및 각페이지 유저이름 보이도록

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const mypageRouter = express.Router();
 const controller = require('../controller/CMypage');
+const multer = require('multer');
+const path = require('path');
+
+// multer 설정
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, ext);
+    const uniqueName = basename + '_' + ext + '_' + Date.now();
+    cb(null, uniqueName);
+  },
+});
+const upload = multer({ storage: storage });
 
 mypageRouter.get('/', controller.mypage);
 
@@ -12,7 +28,7 @@ mypageRouter.get('/mycomment', controller.mycomment);
 //회원 삭제하기 위한 라우터
 mypageRouter.post('/', controller.delete_user);
 //회원정보 수정하기 위한 라우터
-mypageRouter.post('/mypage/update', controller.update_profile);
+mypageRouter.post('/mypage/update', upload.single('profileImage'), controller.update_profile);
 //코멘트 삭제하기 위한 라루터
 mypageRouter.delete('/mycomment/:id', controller.delete_comment);
 //내가 좋아요 누른 코멘트 삭제하기 위한 라우터
