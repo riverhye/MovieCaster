af6c9288e238f537bf5d61f9c5600504ada0db8c
riverhye <uhzigani@gmail.com>
2023-12-08 16:14:56 +0900

    update: 메인 새로고침 시 좋아요 시도

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const controller = require("../controller/CMain");
-const api = require("../controller/CGetMovies");
+const controller = require('../controller/CMain');
+const api = require('../controller/CGetMovies');
 
-router.get("/", controller.main);
+router.get('/', controller.main);
+router.post('/', controller.post_main);
 
 // router.get("/header", controller.header);
 // router.get("/footer", controller.footer);
 
-router.get("/saveMovies", api.get_api_movies);
+router.get('/saveMovies', api.get_api_movies);
 
 module.exports = router;
